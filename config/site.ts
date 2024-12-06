a14cbe30982e081fd0406d3002973d22366ac0e4
baptistesx <baba.om@live.fr>
2024-12-06 14:52:07 +0000

    Deploying to gh-pages from @ Avocalendar/landing@9f14344a7070303ea8d0d4b3d36650744f39ba88 🚀

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Waitlist",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+}
