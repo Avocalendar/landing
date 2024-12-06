0e33ec283c1ccad573b5679e366aa2a703e8ce58
baptistesx <seuxbaptiste@gmail.com>
2024-12-06 12:01:58 +0100

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  output: 'export',
+  basePath: '/landing',
+  images: {
+    unoptimized: true,
   },
 }
 
