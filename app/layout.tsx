5b078880e455e19b347925662338d40c40b4a1e3
baptistesx <seuxbaptiste@gmail.com>
2024-12-06 10:46:23 +0100

    maj UI

---
+++ app/layout.tsx
@@ -0,0 +1,72 @@
+import "@/styles/globals.css"
+import { Metadata } from "next"
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { fontSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import DotPattern from "@/components/magicui/dot-pattern"
+import { SiteHeader } from "@/components/site-header"
+import { ThemeProvider } from "@/components/theme-provider"
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <SiteHeader />
+            <div className="relative flex min-h-screen flex-col py-20 lg:py-0 md:py-0 sm:py-20">
+              <div className="flex-1">{children}</div>
+
+              <DotPattern
+                width={20}
+                height={20}
+                cx={1}
+                cy={1}
+                cr={1}
+                className={cn(
+                  "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+                )}
+              />
+            </div>
+
+            <span className="fixed bottom-4 left-1/2 -translate-x-1/2 text-sm text-muted-foreground">
+              Template de{" "}
+              <Link href="https://www.easyui.pro/ez-waitlist">
+                https://www.easyui.pro/ez-waitlist
+              </Link>
+            </span>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
+}
---
+++ components/site-header.tsx
@@ -0,0 +1,15 @@
+import { MainNav } from "@/components/main-nav"
+import { ThemeToggle } from "@/components/theme-toggle"
+
+export function SiteHeader() {
+  return (
+    <header className="bg-background sticky top-0 z-40 w-full  backdrop-blur">
+      <div className="container flex h-16 text-2xl items-center space-x-4 sm:justify-between sm:space-x-0">
+        <MainNav />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  )
+}
